9251cb4e19e7a57612b39f7efe7cb3052eeda765
haaskrishna18 <haaskrishna18@gmail.com>
2023-10-20 09:22:23 +0200

    Added more cases for filter Added cases to check if image and video filter returns all accounted

--- Test Automation Approval/PageObject/PageActions/MediaLibraryAction.js
+++ Test Automation Approval/PageObject/PageActions/MediaLibraryAction.js
@@ -1,206 +1,249 @@
-const MediaLibraryPageLocators = require("../PageElements/MediaLibrary_elements.json")
+const MediaLibraryPageLocators = require("../PageElements/MediaLibrary_elements.json");
 
 export class MediaLibraryElements {
-   MediaLibraryTAb() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.media_libraryTab)
-         .should('have.text', 'media library')
-         .click()
+  MediaLibraryTAb() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.media_libraryTab
+    )
+      .should("have.text", "media library")
+      .click();
+  }
 
-   }
+  SearchBOx() {
+    cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Search_box)
+      .should("be.visible")
+      .click();
+  }
 
-   SearchBOx() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Search_box)
-         .should('be.visible').click()
+  SearchBTN() {
+    cy.contains(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Search_BTN
+    )
+      .should("have.text", "Search")
+      .click();
+  }
 
-   }
+  SearchQuery(searchquery) {
+    cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Search_box)
+      .click()
+      .clear()
+      .type(searchquery);
+  }
 
-   SearchBTN() {
-      cy.contains(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Search_BTN)
-         .should('have.text', 'Search').click()
+  SelectImage() {
+    cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Select_Image)
+      .should("be.visible")
+      .eq(0)
+      .should("be.visible")
+      .click();
+  }
 
-   }
+  Imagetitle() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Image_Title
+    ).should("have.text", "coconut 1");
+  }
+  Format() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Check_Format
+    ).should("have.text", "JPEG");
+  }
 
-   SearchQuery(searchquery) {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Search_box).click().clear().type(searchquery)
+  FileSize() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.File_Size
+    ).should("have.text", "821 KB");
+  }
+  BackButton() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.backButton
+    ).click();
+  }
 
-   }
+  Resolution() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Resolution
+    ).should("have.text", "1296 x 728 px");
+  }
 
-   SelectImage() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Select_Image).should('be.visible').eq(0).should('be.visible').click()
+  Sizes() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Sizes
+    ).should("have.text", "Small");
+  }
 
-   }
+  Select_Options() {
+    //cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Select_Options).should('be.visible').click();
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Select_Options
+    )
+      .contains("Original")
+      .should("be.visible")
+      .click();
+  }
 
-   Imagetitle() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Image_Title).should('have.text', 'coconut 1');
+  Verify_Download() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Verify_Download
+    ).should("have.text", "  Download ");
+  }
 
-   }
-   Format() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Check_Format).should('have.text', 'JPEG');
+  Download_File() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Download_File
+    ).click();
+  }
 
-   }
+  Filter() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Filter
+    ).should("be.visible");
+  }
 
-   FileSize() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.File_Size).should('have.text', '821 KB');
+  FilterOpen() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Filter
+    ).click();
+  }
+  Option1() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FilterOption1
+    ).should("have.text", "Filetypes");
+  }
+  Option1Click() {
+   cy.get(
+     MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FilterOption1
+   )
+     .contains("Images")
+     .click();
+ }
 
-   }
-   BackButton() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.backButton).click();
+  Option2Click() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FilterOption2
+    )
+      .contains("Movies")
+      .click();
+  }
 
-   }
+  Option2() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FilterOption2
+    ).should("have.text", "Orientations");
+  }
+  FiletypeFilter() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FileType_Filter
+    ).click();
+  }
+  CheckImage() {
+    cy.contains(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Images
+    ).click();
+  }
 
-   Resolution() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Resolution).should('have.text', '1296 x 728 px');
+  OrientationFilter() {
+    cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Orientation)
+      .should("be.visible")
+      .click();
+  }
 
-   }
+  Landscapecheck() {
+    cy.contains(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Landscape
+    )
+      .should("be.visible")
+      .click();
+  }
+  //       cy.wrap(element)
+  //       .invoke('data-w')
+  //       .then(parseInt)
+  //       .then((width)=>{
+  //          cy.wrap(element)
+  //          .invoke('data-h')
+  //          .then(parseInt)
+  //          .then((height)=>{
+  // if (width>height) {
 
-   Sizes() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Sizes).should('have.text', 'Small');
+  // } else {
 
-   }
+  // }
 
-   Select_Options() {
-      //cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Select_Options).should('be.visible').click();
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Select_Options).contains('Original').should('be.visible').click();
+  //          }
+  //       }
 
-   }
+  //    })
+  // })
+  // if (width > height)
+  //    return -1            // return -1 if element potrait
+  // else if (width === height)
+  //    return 0             // return 0 if element is square
+  // else
+  //    return 1             // return 1 if element is landscape
 
-   Verify_Download() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Verify_Download).should('have.text', '  Download ');
+  // .then((yo) => {
+  //    element.should('have.attr', 'data-h')
+  //       .then(() => {
+  //          element.invoke('attr', 'data-h').then((heightValue) => {
+  //             height = Number(heightValue)
+  //             if (width > height)
+  //                return -1            // return -1 if element potrait
+  //             else if (width === height)
+  //                return 0             // return 0 if element is square
+  //             else
+  //                return 1             // return 1 if element is landscape
+  //          })
+  //       })
+  // })
 
-   }
+  async CompareWidthAndHeight(element) {
+    const width = await cy.wrap(element).invoke("attr", "data-w");
+    const height = await cy.wrap(element).invoke("attr", "data-h");
+    return parseInt(width) - parseInt(height);
+  }
 
-   Download_File() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Download_File).click();
+  UpdateCountBasedOnOrientation(difference) {
+    let portraitCount = 0;
+    let landscapeCount = 0;
+    let squareCount = 0;
+    if (difference > 0) {
+      landscapeCount++;
+    } else if (difference < 0) {
+      portraitCount++;
+    } else {
+      squareCount++;
+    }
+    return {
+      portrait: portraitCount,
+      landscape: landscapeCount,
+      square: squareCount,
+    };
+  }
 
-   }
+  GetAssets() {
+    return cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.ListOfAssets
+    );
+  }
+  UsageFilter() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.UsageType
+    ).click();
+  }
 
-   Filter() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Filter).should('be.visible');
+  Digitalcheck() {
+    cy.contains(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Digital
+    ).click();
+  }
 
-   }
+  ClearFilter() {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.ClearFilter
+    ).click();
+  }
 
-   FilterOpen() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Filter).click();
-
-   }
-   Option1() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FilterOption1).should('have.text', 'Filetypes');
-
-   }
-
-   Option2() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FilterOption2).should('have.text', 'Orientations');
-
-   }
-   FiletypeFilter() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.FileType_Filter).click()
-
-   }
-   CheckImage() {
-      cy.contains(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Images).click()
-
-   }
-
-   OrientationFilter() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Orientation).should('be.visible').click()
-
-   }
-
-   Landscapecheck() {
-      cy.contains(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Landscape).should('be.visible').click()
-
-   }
-   //       cy.wrap(element)
-   //       .invoke('data-w')
-   //       .then(parseInt)
-   //       .then((width)=>{
-   //          cy.wrap(element)
-   //          .invoke('data-h')
-   //          .then(parseInt)
-   //          .then((height)=>{
-   // if (width>height) {
-
-   // } else {
-
-   // }
-
-   //          }
-   //       }
-
-
-   //    })
-   // })
-   // if (width > height)
-   //    return -1            // return -1 if element potrait
-   // else if (width === height)
-   //    return 0             // return 0 if element is square
-   // else
-   //    return 1             // return 1 if element is landscape
-
-
-
-
-
-   // .then((yo) => {
-   //    element.should('have.attr', 'data-h')
-   //       .then(() => {
-   //          element.invoke('attr', 'data-h').then((heightValue) => {
-   //             height = Number(heightValue)
-   //             if (width > height)
-   //                return -1            // return -1 if element potrait
-   //             else if (width === height)
-   //                return 0             // return 0 if element is square
-   //             else
-   //                return 1             // return 1 if element is landscape
-   //          })
-   //       })
-   // })
-
-   async CompareWidthAndHeight(element) {
-      const width = await cy.wrap(element).invoke('attr', 'data-w')
-      const height = await cy.wrap(element).invoke('attr', 'data-h')
-      return parseInt(width) - parseInt(height);
-
-
-   }
-
-   UpdateCountBasedOnOrientation(difference) {
-      let portraitCount = 0;
-      let landscapeCount = 0;
-      let squareCount = 0;
-      if (difference > 0) {
-         landscapeCount++;
-      } else if (difference < 0) {
-         portraitCount++;
-      } else {
-         squareCount++;
-      }
-      return {
-         portrait: portraitCount,
-         landscape: landscapeCount,
-         square: squareCount
-      };
-   }
-
-   GetAssets() {
-      return cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.ListOfAssets);
-   }
-   UsageFilter() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.UsageType).click()
-
-   }
-
-   Digitalcheck() {
-      cy.contains(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.Digital).click()
-
-   }
-
-   ClearFilter() {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.ClearFilter).click()
-
-   }
-
-   CountMediaElements() {
+  /*CountMediaElements() {
       // Count the number of image and video elements on the page
       cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.ListOfAssets)
          .should('have.length.gt', 0)
@@ -210,30 +253,53 @@ export class MediaLibraryElements {
             return totalMediaCount;
          })
 
-   }
+   }*/
 
-   DisplayedCount() {
-      // Extract the count from the displayed search result text
-      // cy.scrollTo('top',{ensureScrollable: false});
-      cy.scrollTo('top', { ensureScrollable: false }).then(() => {
-         return cy.wait(4500)
+  CountVideoElements() {
+    // Count the number of image and video elements on the page
+    
+    return cy
+      .get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.ListOfAssets)
+      .find("div > .icon-play.o-library__video-icon")
+      .then(($videoElements) => {
+        return $videoElements.length;
+      });
+  } 
+  CountImageElements() {
+   // Count the number of image and video elements on the page
+   cy.wait(2500)
+   return cy
+     .get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.ListOfAssets)
+     .find("img[alt]")
+     .then(($imgElements) => {
+       return $imgElements.length;
+     });
+ }
 
-      })
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.AssetsCount)
-         .invoke('text')
-         .then((text) => {
-            const match = text.match(/\d+/); // Extract numbers from the text
-            const displayedCount = match ? parseInt(match[0]) : null;
-            // Verify that the displayed count matches the actual count of media assets
-            return displayedCount;
-         })
+  DisplayedCount() {
+    // Extract the count from the displayed search result text
+    // cy.scrollTo('top',{ensureScrollable: false});
+    cy.scrollTo("top", { ensureScrollable: false }).then(() => {
+      return cy.wait(4500);
+    });
+    return cy
+      .get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.AssetsCount)
+      .invoke("text")
+      .then((text) => {
+        const match = text.match(/\d+/); // Extract numbers from the text
+        const displayedCount = match ? parseInt(match[0]) : null;
+        // Verify that the displayed count matches the actual count of media assets
+        return displayedCount;
+      });
+  }
 
-   }
-
-   NoMatchesFound(query) {
-      cy.get(MediaLibraryPageLocators.CreateMediaLibraryPageLocators.NoMatchesFound).should('have.text', `Hm, we can't find any matches for “${query}”.`);
-      cy.get('.tw-p-0 > .tw-font-body').should('have.text', 'Have you checked the spelling? Try a different search term.');
-
-   }
-
+  NoMatchesFound(query) {
+    cy.get(
+      MediaLibraryPageLocators.CreateMediaLibraryPageLocators.NoMatchesFound
+    ).should("have.text", `Hm, we can't find any matches for “${query}”.`);
+    cy.get(".tw-p-0 > .tw-font-body").should(
+      "have.text",
+      "Have you checked the spelling? Try a different search term."
+    );
+  }
 }
--- Test Automation Approval/cypress/e2e/FilterScenario.cy.js
+++ Test Automation Approval/cypress/e2e/FilterScenario.cy.js
@@ -1,6 +1,6 @@
 import { MediaLibraryElements } from "../../PageObject/PageActions/MediaLibraryAction";
 
-const MediaLibrary_Elements = new MediaLibraryElements;
+const MediaLibrary_Elements = new MediaLibraryElements();
 
 describe("Filter Test Cases", () => {
   let testSiteUrls;
@@ -25,7 +25,7 @@ describe("Filter Test Cases", () => {
     cy.visit(testSiteUrls.baseUrl + testSiteUrls.MediaLibrary);
     MediaLibrary_Elements.MediaLibraryTAb();
   });
-
+  // Takes a random query from searchqueries and then
   it("should display the filter element", () => {
     cy.get("@searchQueries").then((queries) => {
       const query = queries[randomQueryIndex];
@@ -39,36 +39,64 @@ describe("Filter Test Cases", () => {
     });
   });
 
+  it("Should count number of videos in filter and compares visible count text", () => {
+    MediaLibrary_Elements.SearchBOx();
+    MediaLibrary_Elements.SearchBTN();
+    MediaLibrary_Elements.FilterOpen();
+    MediaLibrary_Elements.Option2Click();
+    MediaLibrary_Elements.CountVideoElements().then((countVids) => {
+      
+      
+      MediaLibrary_Elements.DisplayedCount().then((DisplayCount) => {
+        expect(countVids).to.equal(DisplayCount);
+      });
+    });
+  });
+  it.only("Should count number of images in filter and compares visible count text", () => {
+    MediaLibrary_Elements.SearchBOx();
+    MediaLibrary_Elements.SearchBTN();
+    cy.scrollTo("bottom", { ensureScrollable: false }).then(() => {
+      return cy.wait(2500);
+    });
+    MediaLibrary_Elements.FilterOpen();
+    MediaLibrary_Elements.Option1Click();
+    
+    MediaLibrary_Elements.CountImageElements().then((countImages) => {
+     
+      MediaLibrary_Elements.DisplayedCount().then((DisplayCount) => {
+        expect(countImages).to.equal(DisplayCount);
+      });
+    });
+  });
+
   it("should open the filter options when the filter button is clicked", () => {
     cy.get("@searchQueries").then((queries) => {
       const query = queries[randomQueryIndex];
-      MediaLibrary_Elements.SearchBOx()
-      MediaLibrary_Elements.SearchBTN()
-      MediaLibrary_Elements.SearchQuery(query)
+      MediaLibrary_Elements.SearchBOx();
+      MediaLibrary_Elements.SearchBTN();
+      MediaLibrary_Elements.SearchQuery(query);
       MediaLibrary_Elements.SearchBTN();
 
       // Click the filter button to open the filter options
-      MediaLibrary_Elements.FilterOpen()
+      MediaLibrary_Elements.FilterOpen();
       // Ensure that the filter options are displayed
-      MediaLibrary_Elements.Option1()
-      MediaLibrary_Elements.Option2()
-
-    })
+      MediaLibrary_Elements.Option1();
+      MediaLibrary_Elements.Option2();
+    });
   });
   it("should filter by Filetypes", () => {
-
-    MediaLibrary_Elements.FilterOpen()
+    MediaLibrary_Elements.FilterOpen();
 
     // Click on the "Filetypes" filter
-    MediaLibrary_Elements.FiletypeFilter()
+    MediaLibrary_Elements.FiletypeFilter();
     // Check the "Images" checkbox
-    MediaLibrary_Elements.CheckImage()
-    cy.wait(1000)
+    MediaLibrary_Elements.CheckImage();
+    cy.wait(1000);
     // Assuming you have applied the filter in a previous step
     // Check if the filter is applied and the value is greater than 0
-    cy.get('span.o-library__filter-selected-label.a-link--color')
-      .should('exist')
-      .invoke('text')
+    cy.get("span.o-library__filter-selected-label.a-link--color")
+      .should("exist")
+      .invoke("text")
       .then((text) => {
         const match = text.match(/\d+/); // Try to match the numeric part
 
@@ -77,30 +105,27 @@ describe("Filter Test Cases", () => {
           cy.expect(filteredCount).to.be.greaterThan(0); // Assert that the count is greater than 0
         } else {
           // Handle the case where no numeric part was found in the text
-          cy.log('No numeric part found in the filter label.');
+          cy.log("No numeric part found in the filter label.");
         }
       });
-  })
-  
+  });
+
   it("should filter by Usage Type", () => {
-    MediaLibrary_Elements.FilterOpen()
-    MediaLibrary_Elements.CheckImage()
+    MediaLibrary_Elements.FilterOpen();
+    MediaLibrary_Elements.CheckImage();
 
     // Click on the "Usage type" filter
-    MediaLibrary_Elements.UsageFilter()
+    MediaLibrary_Elements.UsageFilter();
     // Check the "Print outs" checkbox
-    MediaLibrary_Elements.Digitalcheck()
-
+    MediaLibrary_Elements.Digitalcheck();
 
     // Add additional assertions as needed to verify the filter results
   });
-  it.only("Clear Filter", () => {
-    MediaLibrary_Elements.FilterOpen()
-    MediaLibrary_Elements.CheckImage()
-    cy.wait(1000)
+  it("Clear Filter", () => {
+    MediaLibrary_Elements.FilterOpen();
+    MediaLibrary_Elements.CheckImage();
+    cy.wait(1000);
     // Clear the filter
-    MediaLibrary_Elements.ClearFilter()
-  })
-})
-
-
+    MediaLibrary_Elements.ClearFilter();
+  });
+});
